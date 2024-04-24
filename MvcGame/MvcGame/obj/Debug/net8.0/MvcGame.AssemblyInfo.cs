4410e4ce85b49613c3a9876cabec40500a068c06
OranBrady <oranbrady01@gmail.com>
2024-04-24 10:42:44 +0100

    2FA working

--- MvcGame/MvcGame/obj/Debug/net8.0/MvcGame.AssemblyInfo.cs
+++ MvcGame/MvcGame/obj/Debug/net8.0/MvcGame.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c8b51c6ee21aebf1ab39ed2362ee61b66e9b58")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
